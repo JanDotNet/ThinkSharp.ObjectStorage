e0c060a35a910bdee6b90d40ebbccfd0abe7fe8d
Jan-Niklas Schäfer <Jan-NiklasSchaefer@gmx.de>
2017-02-15 07:26:11 +0100

    added xml documentation

--- ObjectStorage/Location/IStorageLocation.cs
+++ ObjectStorage/Location/IStorageLocation.cs
@@ -1,12 +1,25 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace ThinkSharp.ObjectStorage.Location
 {
     public interface IStorageLocation
     {
+        /// <summary>
+        /// Returns a an opend stream to the underlying data.
+        /// </summary>
+        /// <returns></returns>
         Stream Open();
+
+        /// <summary>
+        /// Writes the content of the passed stream to the storage location.
+        /// </summary>
+        /// <param name="stream"></param>
         void Write(Stream stream);
 
+        /// <summary>
+        /// Clears the storage location.
+        /// </summary>
         void Clear();
     }
 }
